00a89336a5a45bb4ff39401f616260be56ae311a
brianbecker <brianjamesbecker@yahoo.com>
2017-08-17 13:37:21 -0700

    please

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const Alexa = require('alexa-sdk');
+
+var newSessionHandlers = {
+    'LaunchRequest' : function(){
+      var message = 'Hi';
+      var reprompt = 'safdsf';
+      this.response.speak(message).listen(reprompt);
+      this.emit(':responseReady');
+    },
+
+    'AMAZON.HelpIntent' : function () {
+      var message = 'Sorry, your on your own';
+      this.response.speak(message).listen(message);
+      this.emit(':responseReady');
+    },
+
+    'AMAZON.StopIntent' : function() {
+      var message = 'see ya boy';
+      this.response.speak(message);
+      this.emit('responseReady');
+    },
+
+    'AMAZON.CancelIntent' : function () {
+      var message = 'later';
+      this.response.speak(message);
+      this.emit('responseReady');
+    }
+
+  };
+
+export.handler = function (event, context) {
+  var alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(newSessionHandlers);
+  alexa.execute();
+}
